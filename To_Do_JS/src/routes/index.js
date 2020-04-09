40b0d3d37533a6924e740689aa15317e4a64f893
SandovalJr <edubizzle7@gmail.com>
2020-04-09 00:44:10 -0500

    add information in MongoDB

--- To_Do_JS/src/routes/index.js
+++ To_Do_JS/src/routes/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req , res) => {
-res.render('index');
-//   res.send("hola que pex");
+const Task = require("../models/task");
+
+router.get("/", (req, res) => {
+  // Operacion consulta
+  
+
+  res.render("index");
+  //   res.send("hola que pex");
 });
 
+// ruta para agregar
+router.post("/add", async (req, res) => {
+  //   console.log(new Task(req.body));
+  const task = new Task(req.body);
+  //   await es para decirle que hay un nuevo dato y enviar un mensaje
+  await task.save();
+  res.send("recibido");
+});
 module.exports = router;
