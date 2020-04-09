4b1bdfb92ad4d31aeb8a5fad277a3c04750a2392
SandovalJr <edubizzle7@gmail.com>
2020-04-09 14:10:45 -0500

    finalizado

--- To_Do_JS/src/routes/index.js
+++ To_Do_JS/src/routes/index.js
@@ -43,8 +43,15 @@ router.get("/edit/:id", async (req, res) => {
   const { id } = req.params;
   const task = await Task.findById(id);
   res.render("edit", {
-    task
+    task,
   });
+});
+
+// para editarlos
+router.post("/update/:id", async (req, res) => {
+  const { id } = req.params;
+  await Task.update({ _id: id }, req.body);
+  res.redirect("/");
 });
 
 router.get("/delete/:id", async (req, res) => {
